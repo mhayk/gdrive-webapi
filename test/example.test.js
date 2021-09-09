ae6876da5304cc0194f374f4a0d41b5b8fafbd4b
Mhayk Whandson <hi@mhayk.com>
2021-09-09 16:38:11 +0100

    initial unit tests have been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- test/example.test.js
+++
@@ -1,7 +0,0 @@
-import { describe, test, expect } from '@jest/globals'
-
-describe('#Testing 1,2,3', () => {
-    test('#test test', () => {
-        expect(true).toBeTruthy()
-    })
-})
---
+++ test/unit/routes.test.js
@@ -0,0 +1,26 @@
+import { describe, test, expect } from '@jest/globals'
+import Routes from './../../src/routes.js'
+
+describe('#Routes test suite', () => {
+
+    describe('#setSocketInstance', () => {
+        test('setSocket should store io instance', () => {
+            const routes = new Routes()
+            const ioObj = {
+                to: (id) => ioObj,
+                emit: (event, message) => {}
+            }
+
+            routes.setSocketInstance(ioObj)
+            expect(routes.io).toStrictEqual(ioObj)
+        })
+    })
+
+    describe('#handler', () => {
+        test.todo('given an inexistent route it should choose default route')
+        test.todo('it should set any request with CORS enabled')
+        test.todo('given method OPTIONS it should choose options route')
+        test.todo('given method POST it should choose options route')
+        test.todo('given method GET it should choose options route')
+    })
+})
