d44fdc355807cc4c587b501402cc52c5e7cd20df
Mhayk Whandson <hi@mhayk.com>
2021-09-11 21:43:15 +0100

    jest config file has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,28 +1,28 @@
 export default {
   clearMocks: true,
   restoreMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  // collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   coverageReporters: [
-    "text",
-    "lcov"
+    'text',
+    'lcov'
   ],
-  testEnvironment: "node",
+  testEnvironment: 'node',
   coverageThreshold: {
     global: {
-      branchs: 100,
+      branches: 100,
       functions: 100,
       lines: 100,
       statements: 100
     }
   },
   watchPathIgnorePatterns: [
-    "node_modules",
+    'node_modules'
   ],
-  transformIgnorePatterns: ["node_modules"],
+  transformIgnorePatterns: ['node_modules'],
   collectCoverageFrom: [
-    "src/**/*.js",
-    "!src/**/index.js",
+    'src/**/*.js',
+    '!src/**/index.js'
   ]
-};
+}
