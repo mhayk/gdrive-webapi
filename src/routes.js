2567b3cc93d407c481971ba881b7799e3c3573c0
Mhayk Whandson <hi@mhayk.com>
2021-09-11 21:42:52 +0100

    fileHelper has been imported.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,46 @@
-import { logger } from "./logger.js";
+import FileHelper from './fileHelper.js'
+import { logger } from './logger.js'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const defaultDownloadsFolder = resolve(__dirname, '../', 'downloads')
 export default class Routes {
     io
-    consructor() {
+    constructor (downloadsFolder = defaultDownloadsFolder) {
+      this.downloadsFolder = downloadsFolder
+      this.fileHelper = FileHelper
     }
 
-    setSocketInstance(io) {
-        this.io = io
+    setSocketInstance (io) {
+      this.io = io
     }
 
-    async defaultRoute(request, response) {
-        response.end('Hello World!');
+    async defaultRoute (request, response) {
+      response.end('Hello World!')
     }
 
-    async options(request, response) {
-        response.writeHead(204);
-        response.end('Hello World!');
+    async options (request, response) {
+      response.writeHead(204)
+      response.end('Hello World!')
     }
 
-    async post(request, response) {
-        logger.info('ae post')
-        response.end()
+    async post (request, response) {
+      logger.info('ae post')
+      response.end()
     }
 
-    async get(request, response) {
-        logger.info('ae get')
-        response.end()
+    async get (request, response) {
+      const files = await this.fileHelper.getFilesStatus(this.downloadsFolder)
+
+      response.writeHead(200)
+      response.end(JSON.stringify(files))
     }
 
-    handler(request, response) {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        const chosen = this[request.method.toLowerCase()] || this.defaultRoute
+    handler (request, response) {
+      response.setHeader('Access-Control-Allow-Origin', '*')
+      const chosen = this[request.method.toLowerCase()] || this.defaultRoute
 
-        return chosen.apply(this, [request, response])
+      return chosen.apply(this, [request, response])
     }
 }
